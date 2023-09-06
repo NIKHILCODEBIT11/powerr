60d4e66a2cbb94b73ff3145630ec967e442c3d6f
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2023-09-06 23:56:30 +0530

    look once again power and modular

--- power.c
+++ power.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int a;
-    printf("enter a \n",a);
-    scanf("%d",&a);
-    printf("so a is %d \n",a);
+    float a;
     int b;
-    printf("enter b \n",b);
+    printf("enter a \n");
+    scanf("%f",&a);
+    printf("so a is %f \n",a);
+    printf("enter b \n");
     scanf("%d",&b);
     printf("so b is %d \n",b);
-    int power=pow(a,b);
-    printf("so power is %d",power);
+    float power=pow(a,b);
+    printf("so power is %f \n",power);
+    int m=16%3;
+    printf("m is %d",m);
+    return 0;
 }
