7bb1b4083e9cd3aca239ebf4c592c2287fd5a9f8
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2023-09-09 17:12:25 +0530

    using loop for questions

--- homework2.c
+++ homework2.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 // write a program to check if given character is digit or not.
 int main(){
-    int a;
+    char ch;
     printf("enter character :");
-    scanf("%d",&a);
-    if(a>=0||a<0){
+    scanf("%c",&ch);
+    if(ch>='0'||ch<'0'){
         printf("digit \n");
     }
     else{
         printf("not a digit \n");
     }
     printf("thank you \n");
+    // doubt
     return 0;
 }
     
