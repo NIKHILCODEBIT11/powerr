1e0b3625f52303a433e9c83beb86c4203731ca6b
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2023-09-10 00:09:28 +0530

    added

--- questions/question14.c
+++ questions/question14.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
-//print the table of a number input by the user.
 int main(){
+    // keep taking numbers as input from user untill enters an odd number.
     int n;
-    printf("enter number : ");
-    scanf("%d",&n);
-    for(int i=1;i<=10;i++){
-        int product=n*i;
-        printf("%d \n",product);
+    do{
+        printf("enter number : ");
+        scanf("%d",&n);
+        printf("%d \n",n);
+        if(n%2!=0){
+            printf("odd");
+            break;
+        }
     }
+    while(9467);
+    /*WE CAN WRITE ANY NUMBER WITH while BECAUSE WE ARE WRITING NUMBER JUST FOR GETTING TRUE AND ONLY 0 GIVES FALSE
+    REST ALL NUMBER GIVES TRUE.
+    */
     return 0;
 }
