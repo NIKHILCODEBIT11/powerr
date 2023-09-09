a8816045ecb6310f29c17b77eeda517fd03ee06e
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2023-09-09 12:49:54 +0530

    loop counter

---
+++ loopcounterfloat.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    for(float i=0.0;i<=20.0;i++){
+        printf("%f \n",i);
+    }
+    return 0;
+}
---
+++ question10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i;
+    // ++i  increase by 1 then use
+    // i++ use then increase
+    for(i=0;i<=10;++i){
+        printf("%d \n",i);
+    }
+    return 0;
+}
