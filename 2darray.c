e8d8ece65e026dba0c14da5706c5b47ada544c1f
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2023-09-14 18:59:03 +0530

    2darrays

---
+++ 2darray.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int marks[2][3]; // _ _ _ | _ _ _
+    marks[0][0]=90;
+    marks[0][1]=90;
+    marks[0][2]=90;
+    marks[1][0]=90;
+    marks[1][1]=90;
+    marks[1][2]=90;
+    printf("%d",marks[1][0]);
+    return 0;
+
+}
