cc3bc5c6092bfc91038a19afc3e1e15548529080
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2023-09-18 17:12:24 +0530

    questions

--- STRUCT2.C
+++ STRUCT2.C
@@ -22,5 +22,19 @@ int main(){
     printf("student name=%s \n",s1.name);
     printf("student roll=%d \n",s1.roll);
     printf("student cgpa=%f \n",s1.cgpa);
+    struct student s2;
+    s2.roll=1600;
+    s2.cgpa=9.1;
+    strcpy(s2.name,"ronalt");
+    printf("student name=%s \n",s2.name);
+    printf("student roll=%d \n",s2.roll);
+    printf("student cgpa=%f \n",s2.cgpa);
+    struct student s3;
+    s3.roll=1700;
+    s3.cgpa=9.3;
+    strcpy(s3.name,"lucius");
+    printf("student name=%s \n",s3.name);
+    printf("student roll=%d \n",s3.roll);
+    printf("student cgpa=%f \n",s3.cgpa);
     return 0;
 }
