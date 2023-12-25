d4c2b90761693bab31d4b36e5306a619b545738d
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2023-12-25 23:31:55 +0530

    revising C from 25/12/2023

---
+++ format_specifier.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    float b=6.5656556;
+    printf("The value of b is %10.2f.\n",b);
+    printf("The value of b is %-10.2f.",b);
+    return 0;
+}
---
+++ sizeof.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("%lu",sizeof(char)); // lu is unsigned long
+    /*
+    I CAN PUT ANY DATA TYPE INSIDE OF sizeof() 
+    TO KNOW IT'S SIZE.
+    */
+    return 0;
+}
